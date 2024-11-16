7b846d55df005d8f5579bc8d2a8e80aa6a9eaf91
georgekibe <georgekibe500@gmail.com>
2024-11-16 20:04:45 +0300

    Created the first test case, configurations are working well

---
+++ src/test/java/base/TestBase.java
@@ -0,0 +1,78 @@
+package base;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.log4testng.Logger;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class TestBase {
+
+    public static WebDriver webDriver;
+    public static Logger logger = Logger.getLogger(TestBase.class);
+    public static Properties config = new Properties();
+    public static FileInputStream fileInputStream;
+
+
+    @BeforeSuite
+    public void setUp() {
+
+        logger.debug("Test execution starting !!!");
+
+        if (webDriver == null) {
+
+            try {
+                fileInputStream = new FileInputStream(System.getProperty("user.dir") + "\\src\\test\\resources\\properties\\Config.properties");
+            } catch (FileNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+
+            try {
+                config.load(fileInputStream);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+
+            if (config.getProperty("browser").equalsIgnoreCase("chrome")) {
+
+                webDriver = new ChromeDriver();
+            } else if (config.getProperty("browser").equalsIgnoreCase("firefox")) {
+                webDriver = new FirefoxDriver();
+            } else if (config.getProperty("browser").equalsIgnoreCase("edge")) {
+                webDriver = new EdgeDriver();
+            }
+
+        }
+
+        webDriver.get(config.getProperty("siteurl"));
+        logger.debug("Navigated to: " + config.getProperty("siteurl"));
+
+        webDriver.manage().window().maximize();
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    @AfterSuite
+    public void tearDown() {
+        if (webDriver != null) {
+            webDriver.quit();
+        }
+
+        logger.debug("Test execution completed !!!");
+    }
+
+
+}
---
+++ src/test/java/testcases/BankManagerLoginTest.java
@@ -0,0 +1,13 @@
+package testcases;
+
+import base.TestBase;
+import org.testng.annotations.Test;
+
+public class BankManagerLoginTest extends TestBase {
+
+    @Test
+    public void bankingManagerLoginTest(){
+
+        logger.debug("Login successfully executed...");
+    }
+}
